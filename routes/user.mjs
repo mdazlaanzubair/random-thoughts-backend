7e888bc96e958554fd4893e55f03b280d0f1813e
mdazlaanzubair <mdazlaan1996@gmail.com>
2022-09-23 08:55:23 -0700

    user route deleted

--- routes/user.mjs
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import user from "../controllers/user.mjs";
-const user_router = express.Router();
-
-// creating routes
-user_router.get("/user", user);
-
-// returning routes
-export default user_router;
--- server.mjs
+++ server.mjs
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import thought_router from "./routes/thoughts.mjs";
-import user_router from "./routes/user.mjs";
 
 // initializing express app
 const app = express();
@@ -9,7 +8,7 @@ const app = express();
 // middleware(s)
 app.use(cors());
 app.use(express.json());
-app.use([user_router, thought_router]);
+app.use([thought_router]);
 
 const port_num = process.env.PORT || 3000;
 app.listen(port_num, function () {
